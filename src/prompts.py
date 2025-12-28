3637035f7eeef2c720d43da9cf574a98ed89978e
bhagatsingh1729 <156886790+bhagatsingh1729@users.noreply.github.com>
2025-12-28 18:01:41 +0530

    Add RAG_PROMPT for context-based questioning

---
+++ src/prompts.py
@@ -0,0 +1,12 @@
+RAG_PROMPT = """
+You are a helpful assistant. Answer the question using ONLY the context below.
+If the answer is not present in the context, say "I don't know".
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
